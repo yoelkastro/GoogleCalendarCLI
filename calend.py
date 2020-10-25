3a90a45ad508b8597f077d82aafc3153d72aa22b
YoelK <yoelkastro@gmail.com>
2020-10-25 21:39:42 +0000

    Added comments

--- calend.py
+++ calend.py
@@ -11,15 +11,29 @@ import click
 import os
 import subprocess
 
+# The scopes of the calendar API
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def printRequiredOptionText(text):
-	print("No {} given, needs {} to operate.".format(text, text))
 
+'''
+
+Create a new datetime object with the given parameters. Any parameter can be left as None to default to the value in datetime.datetime.now().
+
+day: The day of the date
+month: The month of the date
+year: The year of the date. Will increment by one if the provided date is earlier than today.
+hour: The hour of the date
+minute: The minute of the date
+second: The second of the date. 0 by default
+startDate: Only used if the return value will be the end date of an event. Used to make sure the ending time isn't before the starting time.
+
+'''
 def createDate(day, month, year, hour, minute, second=0, startDate=None):
 
+	# Get the current datetime
 	date = datetime.datetime.now()
 	
+	# Replace the values of the datetime with desired values if they have been provided.
 	if(not day is None):
 		date = date.replace(day=day)
 	if(not month is None):
@@ -32,9 +46,11 @@ def createDate(day, month, year, hour, minute, second=0, startDate=None):
 	date = date.replace(minute=minute)
 	date = date.replace(second=second)
 
+	# If the given date is in the past, increment the year by one if the year wasn't explicitly set
 	if(date < datetime.datetime.now() and date.day != datetime.datetime.now().day and year is None):
 		date = date.replace(year=date.year+1)
 
+	# If the ending time of the event is before the starting time, move the ending date to the next day
 	if(not startDate is None and date < startDate and date + datetime.timedelta(days=1) > startDate):
 		date = date + datetime.timedelta(days=1)
 
@@ -43,6 +59,11 @@ def createDate(day, month, year, hour, minute, second=0, startDate=None):
 
 @click.group()
 @click.pass_context
+'''
+
+Main command. Authenticates API credentials before calling other commands.
+
+'''
 def main(ctx):
 	creds = None
 	ctx.obj = {}
@@ -58,7 +79,12 @@ def main(ctx):
 
 
 @main.command(help="Initialize the command line tool with appropriate credentials")
-@click.argument("credentialdir") #help="The credential file obtained from the Google Calendar API")
+@click.argument("credentialdir")
+'''
+
+Initialization command, has to be called after installation. Requires API credentials for setup.
+
+'''
 def init(credentialdir):
 	flow = InstalledAppFlow.from_client_secrets_file(
 		credentialdir, SCOPES)
@@ -87,12 +113,19 @@ def init(credentialdir):
 
 @click.option("--filename", "-f", help="Creates events based on each line of the given file. Each line must be in the format \"EVENTNAME XXXX YYYY\", where X and Y are the start and end times in military time")
 @click.pass_context
+'''
+
+Adds a new calendar event.
+
+'''
 def add(ctx, name, start_hour, end_hour, start_minute, end_minute, date, month, day, fortnightly, repeat, weekly, daily, filename):
 
+	# The day for the starting time of the generated event
 	date_day = None
 
 	day = day.lower()
 
+	# Make sure conflicting options aren't used together
 	if(not weekly is None and not daily is None):
 		print("\'daily\' option can't be used with the \'weekly\' options")
 		ctx.exit(0)
@@ -105,6 +138,7 @@ def add(ctx, name, start_hour, end_hour, start_minute, end_minute, date, month, 
 		print("\'fortnightly\' option can't be used with the \'repeat\' options")
 		ctx.exit(0)
 
+	# Setup the day of the event. Defaults to TODAY
 	if(date is None):
 		if(day == "today"):
 			date_day = datetime.datetime.today().day
@@ -113,26 +147,33 @@ def add(ctx, name, start_hour, end_hour, start_minute, end_minute, date, month, 
 	else:
 		date_day = date
 
+	# Get the rules for recurring events, if the event is recurring
 	rec = []
 	if(not weekly is None):
 			rec = ['RRULE:FREQ=WEEKLY;COUNT={}'.format(weekly)]
 	if(not daily is None):
 		rec = ['RRULE:FREQ=DAILY;COUNT={}'.format(daily)]
 
+	# The maximum number of repeats of the event. Repeating event are different from recurring events in that recurring events are a function supported by the API while repeating events is just creating the same event on a different day.
 	maxIter = 1
+	# The frequency of the repeats in days
 	freq = 0
 
+	# If repeating fortnightly, set the frequency to 14 days (2 weeks)
 	if(not fortnightly is None):
 		maxIter = fortnightly
 		freq = 14
 
+	# If repeating, set the values to the ones given
 	if(not repeat is None):
 		maxIter = repeat[1]
 		freq = repeat[0]
 
+	# Iterator
 	fnIter = 0
 
 	while(fnIter < maxIter):
+		# If the event isn't being obtained from a file, generate one event
 		if(filename is None):
 
 			event = {
@@ -159,6 +200,7 @@ def add(ctx, name, start_hour, end_hour, start_minute, end_minute, date, month, 
 			ctx.obj["service"].events().insert(calendarId="primary", body=event).execute()
 
 		else:
+		# If the events are being generated from a file, iterate over each line
 			with open(filename) as file:
 				for f in file.read().splitlines():
 					line = f.split(" ")
